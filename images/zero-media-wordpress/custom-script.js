6765ff9e37a93cad40656ef94227978113718792
Studio Wromo Marketplace <info.wromo@gmail.com>
2024-08-25 21:25:33 +0200

    Update custom-script.js

--- images/zero-media-wordpress/custom-script.js
+++ images/zero-media-wordpress/custom-script.js
@@ -1,6 +1,8 @@
 /**
  * Public Active Codes
 **/
+// Instantiate the License Verification Class
+$zmw_license = new ZmwLicenseVerify();
 
 if (!$zmw_license->is_license_active()) {
     add_action('admin_notices', 'zmw_show_license_error');
