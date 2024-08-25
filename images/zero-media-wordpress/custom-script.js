5b4278a31553ba47305af1ad4b7cbefbbb80576c
Studio Wromo Marketplace <info.wromo@gmail.com>
2024-08-25 21:15:00 +0200

    Update custom-script.js

--- images/zero-media-wordpress/custom-script.js
+++ images/zero-media-wordpress/custom-script.js
@@ -5,13 +5,3 @@ public function is_license_active() {
         $isActivated = get_option('wromo_is_activated');
         return $isActivated;
     }
-
-/**
- * RAW PHP Codes
-**/
-require ZMW_PATH . "/incl/php-codes/attachment-fields.php";
-require ZMW_PATH . "/incl/php-codes/generate-bridge-file.php";
-require ZMW_PATH . "/incl/php-codes/license-verify.php";
-require ZMW_PATH . "/incl/php-codes/save-external-media-url.php";
-require ZMW_PATH . "/incl/php-codes/url-replacement.php";
-require ZMW_PATH . "/incl/php-codes/utils.php";
